54b86a9edc3c478a747b268c7b0ed057e0c5458b
SAN-2816 <jinsan2816@gmail.com>
2020-08-05 11:04:26 +0900

    Add .gitignore .idea/dictionaries/san.xml

--- Notimote/src/main/java/com/devsan/notimote/NotimoteView.kt
+++ Notimote/src/main/java/com/devsan/notimote/NotimoteView.kt
@@ -3,11 +3,9 @@ package com.devsan.notimote
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
-import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.os.Build
-import android.view.View
 import android.widget.RemoteViews
 import androidx.core.app.NotificationCompat
 
